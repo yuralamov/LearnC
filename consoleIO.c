e33464e743f9e7ef587d102a7a748bc4dbd17b0f
Yura Lamov <44276956+yuralamov@users.noreply.github.com>
2020-10-11 12:01:47 +0300

    Update consoleIO.c

--- consoleIO.c
+++ consoleIO.c
@@ -1,5 +1,29 @@
-// Ввод-вывод данных в консоли
-#include <stdio.h>
-int main() {
-    
+/*Ввод-вывод основных типов переменных в консоли*/
+
+#include <stdio.h> //Операции ввода-вывода
+#include <locale.h> //Для кодировок, отличных от UTF
+#include <limits.h> //Работа с диапазоними для типов
+
+void type_integer()
+{
+    printf("Целочисленное значение int\n");
+    printf("Размер типа int\t: %d байт\t", sizeof(int));
+    printf("от %d до %d\n", INT_MAX, INT_MIN);
+    int a = 1;
+    printf("В программе  переменной a присвоено значение \"%d\"\n", a);
+    int b;
+    printf("Введите целое число для переменной b\t");
+    scanf("%d", &b);
+    printf("Введеное значение: \"%d\"\n", b);
+    int c = a+b;
+    printf("Сумма %d + %d = %d\n",a,b,c);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    type_integer();
+
+    return 0;
 }
